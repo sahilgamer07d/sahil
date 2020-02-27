e4c06d83a68461368796fc7f57fd6874d8e7d05f
sahilgamer07d <61556793+sahilgamer07d@users.noreply.github.com>
2020-02-27 23:29:22 +0530

    Update on.py

--- stdplugins/on.py
+++ stdplugins/on.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions =       "`i am ON My Mastor \n\nTelethon version: 1.10.10 \n\nPython: 3.8.3 \n--------------------------- \nUser: is the one who is using it \n\nCreator: Mayur Karaniya \n\nOwner: 3Cube TeKnoways \n\nUserbot: testuserbot \n\nDatabase Status: Basiclly Telegram Databases functioning is normal! just sometimes it creates hoax!`"
+    mentions =       "Sahil Ka usar bot hu 24x7 online raheta hu dont woryy!`"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
